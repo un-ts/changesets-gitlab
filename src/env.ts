4917437125ffe74f16f4d7c72681e4d4cec723ad
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-11-17 23:43:40 +0800

    fix(ci):  only check for the token on demand

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,3 @@
-import { setFailed } from '@actions/core'
 import dotenv from 'dotenv'
 
 import type { Env } from './env.types'
@@ -6,17 +5,27 @@ import type { Env } from './env.types'
 const prepareEnv = () => {
   dotenv.config()
 
-  if (!process.env.GITLAB_TOKEN) {
-    setFailed('Please add the `GITLAB_TOKEN` to the changesets action')
-  }
+  return {
+    ...process.env,
 
-  const env = { ...process.env }
-  env.GITLAB_CI_USER_EMAIL ??= '"gitlab[bot]@users.noreply.gitlab.com"'
+    CI_MERGE_REQUEST_IID: +process.env.CI_MERGE_REQUEST_IID!,
+    GITLAB_CI_USER_EMAIL:
+      process.env.GITLAB_CI_USER_EMAIL ??
+      'gitlab[bot]@users.noreply.gitlab.com',
+    GITLAB_COMMENT_TYPE: process.env.GITLAB_COMMENT_TYPE ?? 'discussion',
+    DEBUG_GITLAB_CREDENTIAL: process.env.DEBUG_GITLAB_CREDENTIAL ?? 'false',
 
-  env.GITLAB_COMMENT_TYPE ??= 'discussion'
-  env.DEBUG_GITLAB_CREDENTIAL ??= 'false'
-
-  return env as Env
+    // only check for the token if we are explicitly using it
+    // eslint-disable-next-line sonar/function-name
+    get GITLAB_TOKEN() {
+      if (!process.env.GITLAB_TOKEN) {
+        throw new Error(
+          'Please add the `GITLAB_TOKEN` to the changesets action',
+        )
+      }
+      return process.env.GITLAB_TOKEN
+    },
+  } as Env
 }
 
 export const env = prepareEnv()

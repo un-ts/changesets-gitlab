c565f2cf04a47882b856ef392539df1e58bb0529
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-11-18 00:34:03 +0800

    chore: remove unnecessary function

--- src/env.ts
+++ src/env.ts
@@ -2,30 +2,23 @@ import dotenv from 'dotenv'
 
 import type { Env } from './types'
 
-const prepareEnv = () => {
-  dotenv.config()
+dotenv.config()
 
-  return {
-    ...process.env,
+export const env = {
+  ...process.env,
 
-    CI_MERGE_REQUEST_IID: +process.env.CI_MERGE_REQUEST_IID!,
-    GITLAB_CI_USER_EMAIL:
-      process.env.GITLAB_CI_USER_EMAIL ||
-      'gitlab[bot]@users.noreply.gitlab.com',
-    GITLAB_COMMENT_TYPE: process.env.GITLAB_COMMENT_TYPE ?? 'discussion',
-    DEBUG_GITLAB_CREDENTIAL: process.env.DEBUG_GITLAB_CREDENTIAL ?? 'false',
+  CI_MERGE_REQUEST_IID: +process.env.CI_MERGE_REQUEST_IID!,
+  GITLAB_CI_USER_EMAIL:
+    process.env.GITLAB_CI_USER_EMAIL || 'gitlab[bot]@users.noreply.gitlab.com',
+  GITLAB_COMMENT_TYPE: process.env.GITLAB_COMMENT_TYPE ?? 'discussion',
+  DEBUG_GITLAB_CREDENTIAL: process.env.DEBUG_GITLAB_CREDENTIAL ?? 'false',
 
-    // only check for the token if we are explicitly using it
-    // eslint-disable-next-line sonar/function-name
-    get GITLAB_TOKEN() {
-      if (!process.env.GITLAB_TOKEN) {
-        throw new Error(
-          'Please add the `GITLAB_TOKEN` to the changesets action',
-        )
-      }
-      return process.env.GITLAB_TOKEN
-    },
-  } as Env
-}
-
-export const env = prepareEnv()
+  // only check for the token if we are explicitly using it
+  // eslint-disable-next-line sonar/function-name
+  get GITLAB_TOKEN() {
+    if (!process.env.GITLAB_TOKEN) {
+      throw new Error('Please add the `GITLAB_TOKEN` to the changesets action')
+    }
+    return process.env.GITLAB_TOKEN
+  },
+} as Env

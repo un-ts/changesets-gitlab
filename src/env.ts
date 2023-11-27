cfee0502bf84cb0a81b14d97da6684ebb3837afc
Vincent Taverna <vtaverna@seatgeek.com>
2023-11-27 11:43:39 -0500

    Fix gitlab host fallbacks
    v0.11.1 broke the gitlab host fallbacks when we moved environment variables to its own module. This change reintroduces the gitlab host fallbacks.

--- src/env.ts
+++ src/env.ts
@@ -15,6 +15,7 @@ export const env = {
     process.env.GITLAB_CI_USER_EMAIL || 'gitlab[bot]@users.noreply.gitlab.com',
   GITLAB_COMMENT_TYPE: process.env.GITLAB_COMMENT_TYPE ?? 'discussion',
   DEBUG_GITLAB_CREDENTIAL: process.env.DEBUG_GITLAB_CREDENTIAL ?? 'false',
+  GITLAB_HOST: process.env.GITLAB_HOST ?? process.env.CI_SERVER_URL ?? 'https://gitlab.com',
 
   // only check for the token if we are explicitly using it
   // eslint-disable-next-line sonar/function-name

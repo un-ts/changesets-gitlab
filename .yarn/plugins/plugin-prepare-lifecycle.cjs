22734607f6476dc179ac9b5eab7a2b7c560c6610
Aleksei Reznichenko <rez.alex@gmail.com>
2026-03-21 17:59:10 +0200

    build: remove yarn, prettier, eslint, and legacy tooling configs

--- .yarn/plugins/plugin-prepare-lifecycle.cjs
+++
@@ -1,1 +0,0 @@
-module.exports={name:"plugin-prepare-lifecycle",factory:e=>({hooks:{afterAllInstalled(r){if(!r.topLevelWorkspace.manifest.scripts.get("prepare"))return;e("@yarnpkg/shell").execute("yarn prepare")}}})};

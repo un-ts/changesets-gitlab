07542f3a647461dba41bccef323fbce0f87dabd2
Nikita <dev.nkh@gmail.com>
2026-08-07 15:36:18 +0200

    test: cover exact package directory match

--- test/get-changed-packages.spec.ts
+++ test/get-changed-packages.spec.ts
@@ -1,6 +1,12 @@
 import { isFileInPackageDirectory } from '../src/get-changed-packages.js'
 
 describe('isFileInPackageDirectory', () => {
+  test('matches the package directory itself', () => {
+    expect(isFileInPackageDirectory('packages/ui-kit', 'packages/ui-kit')).toBe(
+      true,
+    )
+  })
+
   test('matches files within the package directory', () => {
     expect(
       isFileInPackageDirectory(
